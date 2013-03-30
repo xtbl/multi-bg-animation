4d70753f845aa5fc4cf0c5efffd156826339bcb5
xtbl <correodecristobal@gmail.com>
2013-03-30 15:09:18 -0600

    assignment 3 ready

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -31,22 +31,24 @@ CRISTOBAL.spaceship = (function ($) {
 		target_difference = 800,
 		target_base = 3000,
 		currentMessage = 0,
+		clickNumber = 0
 
 		config = {
 			spaceShipContainer: $("#main-spaceship"),
 			sceneContainer: $('.multiback'),
-			messageBox: $('#animatedback p')
+			messageBox: $('#animatedback p'),
+			messageSpeed: 20
 		},
 
 		spaceshipMessages = [
 			'Enjoying the view?',
 			'If you click on the spaceship, it will go faster.',
-			'But please, be careful, this new experimental fusion engine is very powerful.',
+			'But please, be careful, the new experimental fusion engine is really powerful.',
 			'Warning. This is extremely dangerous.',
-			':\'(', //&quot;
+			'D A N G E R ! ! !',
 			'Goodbye hero.',
-			'Don\'t worry, the pilot is ok.',
-			'This space adventure was brought to you by: Cristobal Avila'
+			'This space adventure was brought to you by: Cristobal.',
+			'Don\'t worry, the pilot is ok... he just went to... another dimension :D'
 		],
 
 		start_animation = function () {
@@ -72,7 +74,7 @@ CRISTOBAL.spaceship = (function ($) {
 		lightingSpace = function ( bgElem ) {
 			var bgElement = $(bgElem);
 			bgElement.css('background-image','none');
-			setInterval(function() {
+			lightingSpaceInterval = setInterval(function() {
 				if( bgElement.hasClass('dark') ){
 					bgElement.removeClass('dark');
 					bgElement.addClass('bright');
@@ -82,37 +84,67 @@ CRISTOBAL.spaceship = (function ($) {
 					bgElement.addClass('dark');
 				}
 			} ,10 );
-			$('#main-spaceship img').fadeOut(5000);
+			$('#main-spaceship img').fadeOut(5000,function() {
+				config.messageBox.removeClass('warning-text');
+				displayMessages( 6 );
+				msgTimeout = setTimeout(function() {
+					displayMessages( 7 );
+					clearInterval(lightingSpaceInterval);
+					bgElement.addClass('dark');
+				},3000);
+			});
 		},
 
 		displayDangerMsg = function ( msgContainer ) {
 			$(msgContainer).fadeIn(3000);
 		},
 
-		displayMessages = function () {
-		//http://stackoverflow.com/questions/4074399/what-to-choose-for-typewriter-effect-in-javascript
-		//http://jsbin.com/araget/5/edit
-		//http://www.thepixelart.com/create-a-typing-text-effect-using-jquery/
-		//http://stackoverflow.com/questions/13325008/typewriter-effect-with-jquery
-			config.messageBox.text('');
-			config.messageBox.teletype({
-				animDelay: 100,
-				text: spaceshipMessages[currentMessage]
-			});
-			currentMessage+=1;
+		displayMessages = function ( msgNum ) {
+			if( msgNum <= spaceshipMessages.length ){
+				config.messageBox.text('');
+				config.messageBox.teletype({
+					animDelay: config.messageSpeed,
+					text: spaceshipMessages[ msgNum-1 ]
+				});
+			}else{
+				console.log('message num is bigger than spaceshipMessages.length');
+			}
 		},
 		accelerationActions = function () {
-			multiplier *= 1.5; 
-			console.log('multiplier is:' + multiplier);
-			displayMessages();
+			multiplier *= 1.2; 
+			clickNumber += 1;
+			console.log('multiplier: ' + multiplier);
+			console.log('clickNumber: ' + clickNumber);
+			
 			reset_animation();
-			if (multiplier > 15) { 
-				displayDangerMsg('.danger'); 
+			
+			// acceleration events
+			if (clickNumber == 1) {
+				displayMessages( 1 );
 			};
-			if (multiplier > 30) {
-				$('.multiback').css('background-image','url(http://localhost:9000/images/stars_01.gif)');
+			// msg: if you click...
+			if (clickNumber == 2) {
+				displayMessages( 2 );
 			};
-			if (multiplier > 60) {
+			// msg: be careful
+			if (multiplier > 6 && multiplier < 7) { 
+				displayMessages( 3 );
+			};			
+			// msg: warning this is extremely...
+			if (multiplier > 21 && multiplier < 28) { 
+				config.messageBox.removeClass('console-text');
+				config.messageBox.addClass('warning-text');
+				displayMessages( 4 );
+			};
+			// high speed with stars background
+			// msg: warning this is dangerous
+			if (multiplier > 27 && multiplier < 35) {
+				$('.multiback').css('background-image','url(images/stars_01.gif)');
+				displayMessages( 5 );
+			};
+			// msg: goodbye hero in fadeout callback
+			// lighting speed stroboscopic bg, hero disappears
+			if (multiplier > 42) {
 				lightingSpace('.multiback');
 			};
 		},
@@ -129,8 +161,7 @@ CRISTOBAL.spaceship = (function ($) {
 
 		// public API
 		return {
-			init: init,
-			config: config
+			init: init
 		};
 
 }(jQuery));
